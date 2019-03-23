0dc2743d90d2e41848c54d6bc96cabbd95a9bb7e
Eser Ozvataf <eser@ozvataf.com>
2019-03-24 02:46:52 +0300

    Updated dependencies.

--- src/lib/__tests__/test.ts
+++ src/lib/__tests__/test.ts
@@ -4,20 +4,22 @@ import fromNodeStream from '../fromNodeStream';
 import toNodeStream from '../toNodeStream';
 import * as fs from 'fs';
 
-test('copy README.md as README.x.md', async () => {
-    await iterate(
-        fromNodeStream(fs.createReadStream('./README.md'), 512),
-        compose(
-            (item) => {
-                const value = item.data.toString('utf8');
+describe('streams', () => {
+    test('copy README.md as README.x.md', async () => {
+        await iterate(
+            fromNodeStream(fs.createReadStream('./README.md'), 512),
+            compose(
+                (item) => {
+                    const value = item.data.toString('utf8');
 
-                console.log(value);
+                    console.log(value);
 
-                return value;
-            },
-            toNodeStream(fs.createWriteStream('./README.x.md')),
-        ),
-    );
+                    return value;
+                },
+                toNodeStream(fs.createWriteStream('./README.x.md')),
+            ),
+        );
 
-    // expect(result).not.toBe(arr1);
+        // expect(result).not.toBe(arr1);
+    });
 });
